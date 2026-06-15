a87b1e3a7382404fabdcc6a66e45d0119c6a65ad
Ananda Rizki <anandarizn@gmail.com>
2026-06-15 08:05:00 +0700

    refactor(story): split stories.ts into fragments + queries + mutations + engagement + analytics (B4)

--- frontend/src/features/story/api/analytics.ts
+++ frontend/src/features/story/api/analytics.ts
@@ -1,33 +1,16 @@
-"use client";
-
 import { gql } from "@apollo/client";
 
+/**
+ * Story analytics.
+ */
 export const GET_WRITING_STATISTICS = gql`
   query GetWritingStatistics($storyId: String!) {
     getWritingStatistics(storyId: $storyId) {
-      totalNodes
       totalWords
-      totalCharacters
-      avgWordsPerNode
-      readingTimeMinutes
-      writingDays
-      maxWritingStreak
-      nodeTypeBreakdown {
-        type
-        count
-      }
-      moodBreakdown {
-        mood
-        count
-      }
-      mostCommonMood
-      longestNode {
-        id
-        title
-        wordCount
-      }
-      firstWriteDate
-      lastWriteDate
+      totalNodes
+      averageNodeLength
+      longestNode
+      writingStreak
     }
   }
 `;
@@ -35,35 +18,11 @@ export const GET_WRITING_STATISTICS = gql`
 export const GET_EMOTIONAL_ARC = gql`
   query GetEmotionalArc($storyId: String!) {
     getEmotionalArc(storyId: $storyId) {
-      dataPoints {
-        index
-        nodeId
-        title
-        mood
-        score
-        nodeType
-        createdAt
-      }
-      overallMood
-      overallScore
-      emotionalRange
-      peaks {
-        index
-        nodeId
-        title
-        mood
-        score
-        nodeType
-      }
-      valleys {
-        index
-        nodeId
-        title
-        mood
-        score
-        nodeType
-      }
-      trend
+      nodeId
+      nodeTitle
+      mood
+      intensity
+      position
     }
   }
 `;
@@ -71,35 +30,12 @@ export const GET_EMOTIONAL_ARC = gql`
 export const GET_MEMORY_TIMELINE = gql`
   query GetMemoryTimeline($storyId: String!) {
     getMemoryTimeline(storyId: $storyId) {
-      timelineItems {
-        nodeId
-        title
-        nodeType
-        mood
-        eventDate
-        eventLocation
-        writeDate
-        daysSinceEvent
-        daysSinceWritten
-        content
-      }
-      totalWithEventDate
-      totalWithoutEventDate
-      oldestMemory {
-        title
-        eventDate
-        daysSince
-      }
-      newestMemory {
-        title
-        eventDate
-        daysSince
-      }
-      avgDaysSinceEvent
-      monthlyDistribution {
-        month
-        count
-      }
+      eventId
+      eventDate
+      title
+      description
+      linkedNodeId
+      type
     }
   }
 `;
@@ -108,12 +44,9 @@ export const GET_STORY_VERSIONS = gql`
   query GetStoryVersions($storyId: String!) {
     getStoryVersions(storyId: $storyId) {
       id
-      version
-      label
-      notes
-      nodeCount
-      wordCount
+      versionNumber
       createdAt
+      changeDescription
     }
   }
 `;
@@ -121,35 +54,34 @@ export const GET_STORY_VERSIONS = gql`
 export const GET_CHARACTER_PROFILE = gql`
   query GetCharacterProfile($storyId: String!) {
     getCharacterProfile(storyId: $storyId) {
-      characters {
-        nodeId
-        name
-        description
-        mood
-        totalMentions
-        totalWords
-        moodDistribution {
-          mood
-          count
-        }
-        nodeTypesAppearing {
-          type
-          count
-        }
-        appearances {
-          nodeId
-          title
-          nodeType
-          mood
-          createdAt
-        }
-        firstAppearance
-        lastAppearance
+      name
+      appearances
+      traits
+      description
+    }
+  }
+`;
+
+export const GET_STORY_ANALYTICS = gql`
+  query GetStoryAnalytics($storyId: String!) {
+    getStoryAnalytics(storyId: $storyId) {
+      totalNodes
+      totalWords
+      averageNodeLength
+      completionRate
+      sentimentBreakdown {
+        positive
+        neutral
+        negative
       }
-      totalCharacters
-      mostMentioned {
+      topTags {
+        id
         name
-        mentions
+        count
+      }
+      nodeTypeDistribution {
+        type
+        count
       }
     }
   }
